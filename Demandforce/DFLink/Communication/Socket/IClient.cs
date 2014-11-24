ba062d7d3223ed127419de5ee3425acaed83d1a9
okdoso <okdoso@126.com>
2014-11-24 13:47:22 +0800

    11/24/2014 Jack Refactoring the communication

---
+++ Demandforce/DFLink/Communication/Socket/IClient.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IClient.cs" company="Demandforce">
+//   Copyright (c) Demandforce. All rights reserved.
+// </copyright>
+// <summary>
+//   The Client interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Demandforce.DFLink.Communication.Socket
+{
+    using System;
+
+    /// <summary>
+    /// The Client interface.
+    /// </summary>
+    public interface IClient
+    {
+        /// <summary>
+        /// Gets or sets the on get data.
+        /// </summary>
+        Action<string> OnGetData { get; set; }
+
+        /// <summary>
+        /// asynchronous connection
+        /// </summary>
+        void Connect();
+    }
+}
